35317acb5edf3b626286057bbe9c20d95ebda1c3
amirsinai <amir.sinaei84@gmail.com>
2022-06-29 16:28:06 -0700

    Added div and global counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Are we here? </p>
+//         <p>Amir's React Web </p>
+
+//         <p>a new text here</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
 
 function App() {
+  let [counter, changeCounter] = useState(1);
+  window.changeCounter = changeCounter;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{"The counter is " + counter}</h1>
+      <h4>{"The counter is " + counter}</h4>
     </div>
   );
 }
